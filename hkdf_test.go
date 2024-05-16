720116f5afd00c98b15dacbe30e6aa534c9adfd5
treewalkr <suthep@pinroj.com>
2024-05-16 21:29:38 +0700

    Add RFC 5869 test vectors for SHA256 and SHA1

--- hkdf_test.go
+++ hkdf_test.go
@@ -1,6 +1,8 @@
 package hkdf
 
 import (
+	"bytes"
+	"encoding/hex"
 	"testing"
 )
 
@@ -35,5 +37,60 @@ func TestHKDF_Expand(t *testing.T) {
 
 	if len(okm) != length {
 		t.Errorf("OKM length incorrect. Expected %d, got %d", length, len(okm))
+	}
+}
+
+func TestHKDF_SHA256(t *testing.T) {
+	// Test vectors from RFC 5869
+
+	// Test Case 1
+	ikm, _ := hex.DecodeString("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b")
+	salt, _ := hex.DecodeString("000102030405060708090a0b0c")
+	info, _ := hex.DecodeString("f0f1f2f3f4f5f6f7f8f9")
+	length := 42
+	expectedOKM, _ := hex.DecodeString(
+		"3cb25f25faacd57a90434f64d0362f2a" +
+			"2d2d0a90cf1a5a4c5db02d56ecc4c5bf" +
+			"34007208d5b887185865")
+
+	hkdf, err := New(SHA256)
+	if err != nil {
+		t.Fatalf("Failed to create HKDF instance: %v", err)
+	}
+
+	okm, err := hkdf.ExtractAndExpand(salt, ikm, info, length)
+	if err != nil {
+		t.Fatalf("ExtractAndExpand failed: %v", err)
+	}
+
+	if !bytes.Equal(okm, expectedOKM) {
+		t.Errorf("OKM does not match expected value.\nGot:      %x\nExpected: %x", okm, expectedOKM)
+	}
+}
+
+func TestHKDF_SHA1(t *testing.T) {
+	// Test vectors from RFC 5869
+
+	ikm, _ := hex.DecodeString("0b0b0b0b0b0b0b0b0b0b0b")
+	salt, _ := hex.DecodeString("000102030405060708090a0b0c")
+	info, _ := hex.DecodeString("f0f1f2f3f4f5f6f7f8f9")
+	length := 42
+	expectedOKM, _ := hex.DecodeString(
+		"085a01ea1b10f36933068b56efa5ad81" +
+			"a4f14b822f5b091568a9cdd4f155fda2" +
+			"c22e422478d305f3f896")
+
+	hkdfInstance, err := New(SHA1)
+	if err != nil {
+		t.Fatalf("Failed to create HKDF instance: %v", err)
+	}
+
+	okm, err := hkdfInstance.ExtractAndExpand(salt, ikm, info, length)
+	if err != nil {
+		t.Fatalf("ExtractAndExpand failed: %v", err)
+	}
+
+	if !bytes.Equal(okm, expectedOKM) {
+		t.Errorf("OKM does not match expected value.\nGot:      %x\nExpected: %x", okm, expectedOKM)
 	}
 }
